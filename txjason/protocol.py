135b88a33580ed2a093d35f22ce4ac0e99ddbb67
Ivan Kravets <me@ikravets.com>
2017-08-06 23:24:53 +0300

    Provide generic factory with protocol

--- txjason/protocol.py
+++ txjason/protocol.py
@@ -1,8 +1,9 @@
-from twisted.internet import protocol
-import service, client
+from twisted.internet import defer, protocol
+import service
 
 
 class BaseServerFactory(protocol.ServerFactory):
+
     def __init__(self, seperator='.', timeout=None):
         self.service = service.JSONRPCService(timeout)
         self.seperator = seperator
@@ -11,8 +12,25 @@ class BaseServerFactory(protocol.ServerFactory):
         return self.protocol(self.service)
 
     def addHandler(self, handler, namespace=None):
-        handler.addToService(self.service, namespace=namespace, seperator=self.seperator)
+        handler.addToService(
+            self.service, namespace=namespace, seperator=self.seperator)
 
 
 class BaseClientFactory(protocol.ClientFactory):
     pass
+
+
+class JSONRPCServerProtocol(protocol.Protocol):
+
+    def __init__(self, service):
+        self.service = service
+
+    @defer.inlineCallbacks
+    def dataReceived(self, data):
+        result = yield self.service.call(data)
+        if result is not None:
+            self.transport.write(result)
+
+
+class JSONRPCServerFactory(BaseServerFactory):
+    protocol = JSONRPCServerProtocol
